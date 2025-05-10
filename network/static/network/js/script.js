daf477243b5bcfc1952fc5842227d8836758d32f
Erlan Akbaraliev <erlan@erlanakbaraliev.local>
2025-05-10 18:09:37 +0200

    30: Adds SPA functionality; Improves div naming style

---
+++ network/static/network/js/script.js
@@ -0,0 +1,26 @@
+const views = [".main-section", ".profile-section"]
+
+$(document).ready(function() {
+    function main() {
+        setView(views[0])
+        setupNavbarHandlers()
+    }
+    
+    function setView(view) {
+        $.each(views, function(k, v) {
+            $(v).hide()
+        })
+        $(view).show()
+    }
+
+    function setupNavbarHandlers() {
+        $("#navbar-logo").click(function() {
+            setView(views[0])
+        })
+        $("#navbar-profile-btn").click(function() {
+            setView(views[1])
+        })
+    }
+
+    main()
+})
