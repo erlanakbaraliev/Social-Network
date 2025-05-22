146e654c15aadf64ff6f0009e34c0c2184097916
Erlan Akbaraliev <erlan@erlanakbaraliev.local>
2025-05-22 19:04:15 +0200

    #36: (chore) Resolves Local Database and Docker Database issue

--- network/admin.py
+++ network/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Post
 from .models import Follow
 from .models import User
-
+from .models import Post
 
 admin.site.register(User)
-admin.site.register(Post)
 admin.site.register(Follow)
+admin.site.register(Post)
